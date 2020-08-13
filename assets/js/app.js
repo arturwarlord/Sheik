b64a6349d0575124f2b1c71b6e1ef718c493f97e
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2020-08-13 21:56:43 +0500

    sheiK

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+console.log("tabs");
+console.log("modal");
+
+var coll = document.getElementsByClassName("sheik__link--btn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
