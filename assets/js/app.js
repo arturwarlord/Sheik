ba020c060de1d20bef7cf1aae724b1535232eedc
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2020-09-07 15:49:32 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,18 @@
 console.log("tabs");
 console.log("modal");
 
-var coll = document.getElementsByClassName("sheik__link--btn");
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
     } else {
-      content.style.display = "block";
+      dropdownContent.style.display = "block";
     }
   });
-}
-
-
-// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
-function openTab(tabName) {
-    var i, x;
-    x = document.getElementsByClassName("containerTab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
 }
